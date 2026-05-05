897fc6768721b131aca0a40c8cf5ef0b23e03d48
Ralph McCracken <ralph@royaldestinations.com>
2026-05-05 12:09:50 -0500

    added pod and b2b logic

--- main.py
+++ main.py
@@ -21,6 +21,18 @@ def smartsheet_to_dataframe(sheet_id):
 
 
 
+def get_pod_from_amenity_string(row):
+
+    if 'POD' in row['Amenity_Notes']:
+        notes = row['Amenity_Notes'].split(' ')
+        return notes[-2]
+    
+    return None
+
+
+
+
+
 APP_NAME = 'Collaboration Companion'
 
 st.set_page_config(page_title=APP_NAME, page_icon='🤝', layout='wide')
@@ -46,18 +58,36 @@ c1, c2          = st.columns(2)
 start_date      = c1.date_input('Date range start', value=datetime.now())
 end_date        = c2.date_input('Date range end', value=datetime.now())
 
-escapia_file    = c1.file_uploader(label='**Housekeeping Arrival Departure Report - Excel 1 line.csv**', type='csv')
-breezeway_file  = c2.file_uploader(label='**breezeway-task-custom-export.csv** | Tasks > All Tasks > All tasks (collab ops reporting)', type='csv')
+c1, c2, c3      = st.columns(3)
+
+escapia_file    = c1.file_uploader(label='**Housekeeping Arrival Departure Report - Excel 1 line.csv**\n\n Download button above', type='csv')
+breezeway_file  = c2.file_uploader(label='**breezeway-task-custom-export.csv**\n\nBreezeway > Tasks > All Tasks > All tasks (collab ops reporting)', type='csv')
+pod_file        = c3.file_uploader(label='**Amenity String Report.csv**\n\nEscapia > Units > Reports > Amenity String Report > (Select all) > CSV', type='csv')
 
 
-if escapia_file and breezeway_file:
+if escapia_file and breezeway_file and pod_file:
     gdf = smartsheet_to_dataframe(st.secrets['smartsheet']['sheet_id']['geo'])
     
     edf = pd.read_csv(escapia_file)
     bdf = pd.read_csv(breezeway_file)
+    pdf = pd.read_csv(pod_file)
 
-    edf = edf[['Unit_Code','Reservation_Number','ReservationTypeDescription','Start_Date']]
+    pdf['Amenity_Notes'] = pdf['Amenity_Notes'].fillna('')
+    pdf['Amenity_Notes'] = pdf['Amenity_Notes'].str.upper()
+    pdf['Pod']           = pdf.apply(get_pod_from_amenity_string, axis=1)
+    pdf                  = pdf[['Unit_Code','Pod']]
+
     edf['Start_Date'] = pd.to_datetime(edf['Start_Date'])
+    edf['PreviousBookingDate'] = pd.to_datetime(edf['PreviousBookingDate'])
+    edf['Previous_Departure_Date'] = edf['PreviousBookingDate'] + pd.Timedelta(days=1)
+    edf = edf[['Unit_Code','Reservation_Number','ReservationTypeDescription','Start_Date','Previous_Departure_Date']]
+
+    def determine_b2b(row):
+        if pd.isna(row['Previous_Departure_Date']):
+            return False
+        return row['Start_Date'] == row['Previous_Departure_Date']
+    
+    edf['isB2B'] = edf.apply(determine_b2b, axis=1)
 
     edf = edf[(edf['Start_Date'] >= pd.to_datetime(start_date)) & (edf['Start_Date'] <= pd.to_datetime(end_date))]
     edf = edf.sort_values(by='Start_Date', ascending=True)
@@ -75,9 +105,12 @@ if escapia_file and breezeway_file:
     gdf = gdf[['Unit_Code','Address','Order']]
 
     df = pd.merge(df, gdf, on='Unit_Code', how='left')
+    df = df.merge(pdf, on='Unit_Code', how='left')
     df = df.sort_values(by=['Start_Date','Order'], ascending=True)
     
-    def print_results(df):
+    def print_results(df, pod_filter=None):
+
+        df = df[df['Pod'] == pod_filter] if pod_filter and pod_filter != 'All' else df
 
         date = ''
         unit = ''
@@ -89,11 +122,20 @@ if escapia_file and breezeway_file:
             
             if row['Unit_Code'] != unit:
                 if 'Renter' in row['ReservationTypeDescription']:
-                    st.header(f'🏠 {row['Unit_Code']} - {row['Address']} - {row['ReservationTypeDescription']} - {row['Start_Date'].strftime('%m/%d')}')
+                    if row['isB2B']:
+                        st.header(f'🏠 {row['Unit_Code']} - {row['Address']} - {row['ReservationTypeDescription']} - {row['Start_Date'].strftime('%m/%d')} - B2B 🔁')
+                    else:
+                        st.header(f'🏠 {row['Unit_Code']} - {row['Address']} - {row['ReservationTypeDescription']} - {row['Start_Date'].strftime('%m/%d')}')
                 elif 'Guest of Owner' in row['ReservationTypeDescription']:
-                    st.header(f'🏡 {row['Unit_Code']} - {row['Address']} - {row['ReservationTypeDescription']} - {row['Start_Date'].strftime('%m/%d')}')
+                    if row['isB2B']:
+                        st.header(f'🏡 {row['Unit_Code']} - {row['Address']} - {row['ReservationTypeDescription']} - {row['Start_Date'].strftime('%m/%d')} - B2B 🔁')
+                    else:
+                        st.header(f'🏡 {row['Unit_Code']} - {row['Address']} - {row['ReservationTypeDescription']} - {row['Start_Date'].strftime('%m/%d')}')
                 else:
-                    st.header(f'🏰 {row['Unit_Code']} - {row['Address']} - {row['ReservationTypeDescription']} - {row['Start_Date'].strftime('%m/%d')}')
+                    if row['isB2B']:
+                        st.header(f'🏰 {row['Unit_Code']} - {row['Address']} - {row['ReservationTypeDescription']} - {row['Start_Date'].strftime('%m/%d')} - B2B 🔁')
+                    else:
+                        st.header(f'🏰 {row['Unit_Code']} - {row['Address']} - {row['ReservationTypeDescription']} - {row['Start_Date'].strftime('%m/%d')}')
 
                 st.write(f'')
                 unit = row['Unit_Code']
@@ -103,5 +145,6 @@ if escapia_file and breezeway_file:
     st.success('Opportunities are sorted by **arrival date**, then by **geographical order**.', icon='🥇')
     st.info('Only tasks with due dates **on or before** each property\'s **arrival date** are included.', icon='🗓️')
 
+    st.selectbox('Filter by POD', options=['All'] + sorted(df['Pod'].unique()), key='pod_filter')
     with st.expander('Opportunities', expanded=False):
-        print_results(df)
+        print_results(df, pod_filter=st.session_state.get('pod_filter'))
